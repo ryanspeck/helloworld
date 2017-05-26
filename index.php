43eb175051b50444912ccf03d973a0cc0f3364f4
U-CLG\rspeck <rspeck@RSPECK-201704.clg.local>
2017-05-26 09:12:03 -0400

    added index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	$DB = mysqli_connect('localhost','user','password','hellodb');
+
+	$query = "select value
+		from integers
+		where word = 'five'";
+
+	$result = $DB->query($query);
+	$result = $result->fetch_assoc();
+
+	echo $result['value'];	
+
+?>
