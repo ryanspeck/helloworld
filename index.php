631f328bddebffe177f918ae61432b7a637478c0
U-CLG\rspeck <rspeck@RSPECK-201704.clg.local>
2017-05-26 09:37:13 -0400

    mm

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-	$DB = mysqli_connect('localhost','user','password','hellodb');
+	$DB = mysqli_connect('localhost','user','password','hellodb')
+		or die("Error " . mysqli_error($DB));
 //	if (mysqli_connect_errno()) {
 //		echo "Failed ". mysqli_connect_error();
 //	}
